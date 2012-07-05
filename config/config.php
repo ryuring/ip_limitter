c75e729f2284a7e00bf41f99e580df49b448d128
ryuring <ryuring55@gmail.com>
2012-07-05 15:54:41 +0900

    プラグインの管理システムへのリンクを配列での表現に調整

--- config/config.php
+++ config/config.php
@@ -21,5 +21,5 @@ $title = 'IPリミッター';
 $description = 'IPアドレスを指定してコンテンツへのアクセスを制限します。';
 $author = 'ryuring';
 $url = 'http://www.e-catchup.jp/';
-$adminLink = '/admin/ip_limitter/ip_limitter_configs';
+$adminLink = array('plugin' => 'ip_limitter', 'controller' => 'ip_limitter_configs', 'action' => 'index');
 $installMessage = '';
