bb5f28813b20f214c29ebbc541a04a5c17840cdd
ryuring <ryuring55@gmail.com>
2014-03-24 14:47:08 +0900

    スキーマデータをアップデート

--- Config/sql/ip_limitter_configs.php
+++ Config/sql/ip_limitter_configs.php
@@ -2,20 +2,20 @@
 /* SVN FILE: $Id: ip_limitter_configs.php 192 2011-06-04 15:53:54Z ryuring $ */
 /* IpLimitterConfigs schema generated on: 2011-06-03 23:06:38 : 1307112218*/
 class IpLimitterConfigsSchema extends CakeSchema {
-	var $name = 'IpLimitterConfigs';
+	public $name = 'IpLimitterConfigs';
 
-	var $file = 'ip_limitter_configs.php';
+	public $file = 'ip_limitter_configs.php';
 
-	var $connection = 'plugin';
+	public $connection = 'plugin';
 
-	function before($event = array()) {
+	public function before($event = array()) {
 		return true;
 	}
 
-	function after($event = array()) {
+	public function after($event = array()) {
 	}
 
-	var $ip_limitter_configs = array(
+	public $ip_limitter_configs = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
 		'name' => array('type' => 'string', 'null' => true, 'default' => NULL),
 		'value' => array('type' => 'text', 'null' => true, 'default' => NULL),
